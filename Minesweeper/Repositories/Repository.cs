4388fdb4ceae186d40ce6b4611dde8a096896a58
Mlendea Horatiu <Mlendea.Horatiu@GMail.com>
2016-06-07 22:22:59 +0300

    Documented Repository and RepositoryException

--- Minesweeper/Repositories/Repository.cs
+++ Minesweeper/Repositories/Repository.cs
@@ -4,20 +4,38 @@ using Minesweeper.Models;
 
 namespace Minesweeper.Repositories
 {
+    /// <summary>
+    /// Repository.
+    /// </summary>
     public class Repository<T> where T : EntityBase
     {
+        /// <summary>
+        /// Gets or sets the entities.
+        /// </summary>
+        /// <value>The entities.</value>
         protected List<T> Entities { get; set; }
 
+        /// <summary>
+        /// Gets the size.
+        /// </summary>
+        /// <value>The size.</value>
         public int Size
         {
             get { return Entities.Count; }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Minesweeper.Repositories.Repository`1"/> class.
+        /// </summary>
         public Repository()
         {
             Entities = new List<T>();
         }
 
+        /// <summary>
+        /// Add the specified entity.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
         public virtual void Add(T entity)
         {
             if (Entities.Contains(entity))
@@ -26,6 +44,10 @@ namespace Minesweeper.Repositories
             Entities.Add(entity);
         }
 
+        /// <summary>
+        /// Get the specified id.
+        /// </summary>
+        /// <param name="id">Identifier.</param>
         public virtual T Get(string id)
         {
             T entity = Entities.Find(E => E.Id == id);
@@ -36,17 +58,29 @@ namespace Minesweeper.Repositories
             return entity;
         }
 
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <returns>The all.</returns>
         public virtual List<T> GetAll()
         {
             return Entities;
         }
 
+        /// <summary>
+        /// Update the specified entity.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
         public virtual void Update(T entity)
         {
             T oldEntity = Get(entity.Id);
             oldEntity = entity; // TODO Real update - currently it is being done by the service
         }
 
+        /// <summary>
+        /// Remove the specified entity.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
         public virtual void Remove(T entity)
         {
             if (!Contains(entity))
@@ -55,6 +89,10 @@ namespace Minesweeper.Repositories
             Entities.Remove(entity);
         }
 
+        /// <summary>
+        /// Remove the specified id.
+        /// </summary>
+        /// <param name="id">Identifier.</param>
         public virtual void Remove(string id)
         {
             if (!Contains(id))
@@ -63,16 +101,27 @@ namespace Minesweeper.Repositories
             Entities.RemoveAll(T => T.Id == id);
         }
 
+        /// <summary>
+        /// Clear this instance.
+        /// </summary>
         public virtual void Clear()
         {
             Entities.Clear();
         }
 
+        /// <summary>
+        /// Contains the specified entity.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
         public virtual bool Contains(T entity)
         {
             return Entities.Find(E => E.Equals(entity)) != null;
         }
 
+        /// <summary>
+        /// Contains the specified id.
+        /// </summary>
+        /// <param name="id">Identifier.</param>
         public virtual bool Contains(string id)
         {
             return Entities.Find(E => E.Id == id) != null;
