72221648c7655cbed9b6b84f78e1b9cf74fc4ef7
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-07-02 10:55:15 +0300

    gfx: Refactoring

--- Minesweeper/Gui/Controls/GuiGameBoard.cs
+++ Minesweeper/Gui/Controls/GuiGameBoard.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-using NuciXNA.DataAccess.Content;
+using NuciXNA.Graphics.Drawing;
 using NuciXNA.Gui.Controls;
 using NuciXNA.Primitives;
 
@@ -15,17 +15,17 @@ namespace Minesweeper.Gui.Controls
 {
     public class GuiGameBoard(IGameManager game) : GuiControl
     {
-        static readonly Color[] DangerColors =
+        static readonly Colour[] DangerColors =
         [
-            Color.Black,   // 0 - unused
-            Color.Blue,    // 1
-            Color.Green,   // 2
-            Color.Red,     // 3
-            Color.Navy,    // 4
-            Color.Maroon,  // 5
-            Color.Purple,  // 6
-            Color.Black,   // 7
-            Color.Gray,    // 8
+            Colour.Black,   // 0 - unused
+            Colour.Blue,    // 1
+            Colour.Green,   // 2
+            Colour.Red,     // 3
+            Colour.Navy,    // 4
+            Colour.Maroon,  // 5
+            Colour.Purple,  // 6
+            Colour.Black,   // 7
+            Colour.Gray,    // 8
         ];
 
         static readonly Rectangle2D UnrevealedSourceRect = new(0, 0, 512, 512);
@@ -34,27 +34,29 @@ namespace Minesweeper.Gui.Controls
         readonly IGameManager game = game;
 
         GuiImage[,] tileImages;
-        Texture2D mineTexture;
-        Texture2D flagTexture;
-        SpriteFont dangerFont;
+        GuiImage[,] flagImages;
+        GuiImage[,] mineImages;
+        GuiText[,] dangerTexts;
 
         protected override void DoLoadContent()
         {
             int tileSize = GameDefines.MapTileSize;
             int tableSize = game.TableSize;
-
-            mineTexture = NuciContentManager.Instance.LoadTexture2D("Tiles/mine");
-            flagTexture = NuciContentManager.Instance.LoadTexture2D("Tiles/flag");
-            dangerFont = NuciContentManager.Instance.LoadSpriteFont("Fonts/InfoBarFont");
+            int iconPad = tileSize / 8;
+            int iconSize = tileSize * 3 / 4;
 
             tileImages = new GuiImage[tableSize, tableSize];
-            List<GuiImage> imageList = new(tableSize * tableSize);
+            flagImages = new GuiImage[tableSize, tableSize];
+            mineImages = new GuiImage[tableSize, tableSize];
+            dangerTexts = new GuiText[tableSize, tableSize];
+
+            List<IGuiControl> children = new(tableSize * tableSize * 4);
 
             for (int y = 0; y < tableSize; y++)
             {
                 for (int x = 0; x < tableSize; x++)
                 {
-                    GuiImage image = new()
+                    tileImages[x, y] = new GuiImage
                     {
                         Id = $"{Id}_tile_{x}_{y}",
                         ContentFile = "Tiles/tiles",
@@ -63,12 +65,40 @@ namespace Minesweeper.Gui.Controls
                         SourceRectangle = UnrevealedSourceRect
                     };
 
-                    tileImages[x, y] = image;
-                    imageList.Add(image);
+                    flagImages[x, y] = new GuiImage
+                    {
+                        Id = $"{Id}_flag_{x}_{y}",
+                        ContentFile = "Tiles/flag",
+                        Size = new Size2D(iconSize, iconSize),
+                        Location = new Point2D(x * tileSize + iconPad, y * tileSize + iconPad)
+                    };
+
+                    mineImages[x, y] = new GuiImage
+                    {
+                        Id = $"{Id}_mine_{x}_{y}",
+                        ContentFile = "Tiles/mine",
+                        Size = new Size2D(iconSize, iconSize),
+                        Location = new Point2D(x * tileSize + iconPad, y * tileSize + iconPad)
+                    };
+
+                    dangerTexts[x, y] = new GuiText
+                    {
+                        Id = $"{Id}_danger_{x}_{y}",
+                        FontName = "InfoBarFont",
+                        Size = new Size2D(tileSize, tileSize),
+                        Location = new Point2D(x * tileSize, y * tileSize),
+                        HorizontalAlignment = Alignment.Middle,
+                        VerticalAlignment = Alignment.Middle
+                    };
+
+                    children.Add(tileImages[x, y]);
+                    children.Add(flagImages[x, y]);
+                    children.Add(mineImages[x, y]);
+                    children.Add(dangerTexts[x, y]);
                 }
             }
 
-            RegisterChildren(imageList);
+            RegisterChildren(children);
         }
 
         protected override void DoUnloadContent() { }
@@ -82,60 +112,35 @@ namespace Minesweeper.Gui.Controls
                 for (int x = 0; x < tableSize; x++)
                 {
                     Tile tile = game.GetTile(x, y);
+
                     tileImages[x, y].SourceRectangle = tile.Cleared ? ClearedSourceRect : UnrevealedSourceRect;
+
+                    SetVisibility(flagImages[x, y], !tile.Cleared && tile.Flagged);
+                    SetVisibility(mineImages[x, y], !tile.Cleared && !tile.Flagged && tile.Mined && !game.Alive);
+
+                    bool showDanger = tile.Cleared && tile.DangerLevel > 0;
+                    SetVisibility(dangerTexts[x, y], showDanger);
+
+                    if (showDanger)
+                    {
+                        dangerTexts[x, y].Text = tile.DangerLevel.ToString();
+                        dangerTexts[x, y].ForegroundColour = DangerColors[tile.DangerLevel];
+                    }
                 }
             }
         }
 
-        protected override void DoDraw(SpriteBatch spriteBatch)
+        protected override void DoDraw(SpriteBatch spriteBatch) { }
+
+        static void SetVisibility(IGuiControl control, bool visible)
         {
-            int tileSize = GameDefines.MapTileSize;
-            int tableSize = game.TableSize;
-
-            for (int y = 0; y < tableSize; y++)
+            if (visible)
             {
-                for (int x = 0; x < tableSize; x++)
-                {
-                    Tile tile = game.GetTile(x, y);
-                    int px = x * tileSize;
-                    int py = GameDefines.InfoBarHeight + y * tileSize;
-
-                    if (tile.Cleared)
-                    {
-                        if (tile.DangerLevel > 0)
-                        {
-                            Color numColor = DangerColors[tile.DangerLevel];
-                            string numText = tile.DangerLevel.ToString();
-                            Vector2 textSize = dangerFont.MeasureString(numText);
-                            Vector2 textPos = new(
-                                px + (tileSize - textSize.X) / 2f,
-                                py + (tileSize - textSize.Y) / 2f);
-
-                            spriteBatch.DrawString(dangerFont, numText, textPos, numColor);
-                        }
-                    }
-                    else
-                    {
-                        if (tile.Flagged)
-                        {
-                            Rectangle flagRect = new(
-                                px + tileSize / 8,
-                                py + tileSize / 8,
-                                tileSize * 3 / 4,
-                                tileSize * 3 / 4);
-                            spriteBatch.Draw(flagTexture, flagRect, Color.White);
-                        }
-                        else if (tile.Mined && !game.Alive)
-                        {
-                            Rectangle mineRect = new(
-                                px + tileSize / 8,
-                                py + tileSize / 8,
-                                tileSize * 3 / 4,
-                                tileSize * 3 / 4);
-                            spriteBatch.Draw(mineTexture, mineRect, Color.White);
-                        }
-                    }
-                }
+                control.Show();
+            }
+            else
+            {
+                control.Hide();
             }
         }
     }
